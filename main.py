97d24875e523d37720ad5e9aac5bc9ef58620618
Sergio Gomez <sergioferg.2003@gmail.com>
2026-06-05 17:12:40 -0400

    leer argumentos de linea de comandos

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import argparse
+import sys
 
 def main() -> None:
+    if len(sys.argv) <= 5:
+        print("StreamUCV D/D")
+        print('Uso: python main.py <server> <bd> <usuario> <password> <driver>')
+        print('Ejemplo: python main.py localhost StreamUCV sa tu_password "ODBC Driver 17 for SQL Server"')
+        return
     parser = argparse.ArgumentParser(description="StreamUCV D/D")
-    parser.add_argument("usuario", type=str, help="Nombre de usuario de SQLServer")
+    parser.add_argument("server", type=str, help="Servidor o instancia de SQL Server")
+    parser.add_argument("bd", type=str, help="Base de Datos")
+    parser.add_argument("user", type=str, help="Usuario")
+    parser.add_argument("pw", type=str, help="Contraseña")
+    parser.add_argument("driver", type=str, help="Driver de conexion")
     args = parser.parse_args()
     
-    print(args.usuario)
+    print(args.server)
+    print(args.bd)
+    print(args.user)
+    print(args.pw)
+    print(args.driver)
 
 if __name__ == "__main__":
     main()
