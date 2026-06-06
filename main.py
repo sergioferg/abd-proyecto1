ba2eeff96a2a31c1120a79889a31a6a1eea5bbfe
Sergio Gomez <sergioferg.2003@gmail.com>
2026-06-06 10:21:53 -0400

    conexion con BD

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 import argparse
 import sys
+import pyodbc
 
 def main() -> None:
-    if len(sys.argv) <= 5:
-        print("StreamUCV D/D")
-        print('Uso: python main.py <server> <bd> <usuario> <password> <driver>')
-        print('Ejemplo: python main.py localhost StreamUCV sa tu_password "ODBC Driver 17 for SQL Server"')
-        return
     parser = argparse.ArgumentParser(description="StreamUCV D/D")
     parser.add_argument("server", type=str, help="Servidor o instancia de SQL Server")
-    parser.add_argument("bd", type=str, help="Base de Datos")
+    parser.add_argument("db", type=str, help="Base de Datos")
     parser.add_argument("user", type=str, help="Usuario")
     parser.add_argument("pw", type=str, help="Contraseña")
     parser.add_argument("driver", type=str, help="Driver de conexion")
     args = parser.parse_args()
+
+    connection = []
     
-    print(args.server)
-    print(args.bd)
-    print(args.user)
-    print(args.pw)
-    print(args.driver)
+    #SQL_CONNECTION_STRING="Driver={<driver>};Server=<server_name>;Database=<database_name>;UId=<username>;Pwd=<password>"
+
+    connection = [
+        f"Driver={{{args.driver}}}",
+        f"Server={args.server}",
+        f"Database={args.db}",
+        f"UId={args.user}",
+        f"Pwd={args.pw}",
+    ]
+    
+    connection_string = ";".join(connection) + ";"
+    
+
+    try: 
+        conn = pyodbc.connect(connection_string)
+        print("Conexión exitosa!")
+        print(connection_string)
+        conn.close()
+    except Exception as e:
+        print(f"Error: {e}")
+
+    
 
 if __name__ == "__main__":
     main()
